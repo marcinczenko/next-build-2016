aab8527378b79557bc16a5c3d749205187a44a02
Marcin Czenko <marcin.czenko@philips.com>
2016-04-15 23:32:15 +0200

    Added AppBar

--- app/js/Main.js
+++ app/js/Main.js
@@ -1,13 +1,22 @@
 import React from 'react'
 
+import getMuiTheme from 'material-ui/styles/getMuiTheme'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import AppBar from 'material-ui/AppBar'
+
 class Main extends React.Component {
 
     render() {
         
         return (
-            <div className="row">
-                <p>Let's code!</p>
-            </div>
+            <MuiThemeProvider muiTheme={getMuiTheme()}>
+                <div className="row">
+                    <AppBar title="Next Build 2016"
+                            showMenuIconButton={false}
+                    />
+                </div>
+            </MuiThemeProvider>
+
         );
     }
 }
